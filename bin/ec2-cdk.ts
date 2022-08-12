ce65fd925b7613633075eab95385cd4c91b9dc5b
Andrew Kirchmyer <akirchmyer@gmail.com>
2022-08-11 23:16:02 -0400

    pull cdk ec2 from npm

--- bin/ec2-cdk.ts
+++ bin/ec2-cdk.ts
@@ -1,8 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import * as path from 'path';
-import { Ec2CdkStack } from '../lib/ec2-cdk-stack';
+import { Ec2CdkStack } from 'simple-cdk-ec2'
 
 const app = new cdk.App();
-
 const userDataPath = path.join(__dirname, '../src/config.sh');
-new Ec2CdkStack(app, 'VerdaccioEc2CdkStack', {}, userDataPath);
+
+new Ec2CdkStack(app, 'VerdaccioEc2CdkStack', {}, {
+    userDataPath
+});

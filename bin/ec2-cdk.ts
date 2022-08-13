96bc04bbbeb0744e8ec26896b46ecd87717b0b64
Andrew Kirchmyer <akirchmyer@gmail.com>
2022-08-13 12:35:50 -0400

    added prettier

--- bin/ec2-cdk.ts
+++ bin/ec2-cdk.ts
@@ -1,10 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import * as path from 'path';
-import { Ec2CdkStack } from 'simple-cdk-ec2'
+import * as cdk from "aws-cdk-lib";
+import * as path from "path";
+import { Ec2CdkStack } from "simple-cdk-ec2";
 
 const app = new cdk.App();
-const userDataPath = path.join(__dirname, '../src/config.sh');
+const userDataPath = path.join(__dirname, "../src/config.sh");
 
-new Ec2CdkStack(app, 'VerdaccioEc2CdkStack', {}, {
-    userDataPath
-});
+new Ec2CdkStack(
+  app,
+  "VerdaccioEc2CdkStack",
+  {},
+  {
+    userDataPath,
+  }
+);
